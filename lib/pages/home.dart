8a1b5fb5cdb0ff3cbcb3c73eb2780b098acc8546
karimax2 <lazaarworks@gmail.com>
2020-07-14 18:31:35 +0100

    Google signin signout setup

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -9,8 +12,50 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool isAuth = false;
 
-  buildAuthScfreen() {
-    return Text('Authenticated');
+// an active listner provided by google to check if the user is authenticated
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handlSignIn(account);
+    }, onError: (err) {
+      print('Error signing in: $err');
+    });
+    // reauthenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handlSignIn(account);
+    }).catchError((err) {
+      print('Error signing in: $err');
+    });
+  }
+
+  handlSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in!: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+// this function to open screen model for auth
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout() {
+    googleSignIn.signOut();
+  }
+
+  buildAuthScreen() {
+    return RaisedButton(
+      child: Text('Logout'),
+      onPressed: logout,
+    );
   }
 
   buildUnAuthScreen() {
@@ -38,7 +83,7 @@ class _HomeState extends State<Home> {
                     color: Colors.white),
               ),
               GestureDetector(
-                onTap: () => print('taped'),
+                onTap: login,
                 child: Container(
                   width: 260.0,
                   height: 60.0,
@@ -57,6 +102,6 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return isAuth ? buildAuthScfreen() : buildUnAuthScreen();
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
