65c3850c8a8922bb61863c6802d62d2df06709e6
karimax2 <lazaarworks@gmail.com>
2020-07-16 20:44:56 +0100

    searach bug

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,9 +11,102 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResultFuture;
+
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = userRef
+        .where('displayName', isGreaterThanOrEqualTo: query)
+        .getDocuments();
+    setState(() {
+      searchResultFuture = users;
+    });
+  }
+
+  clearSearch() {
+    searchController.clear();
+  }
+
+  AppBar buildSearchFiedl() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: 'Search for user..',
+          filled: true,
+          prefix: Icon(
+            Icons.account_box,
+            size: 28.0,
+            color: Theme.of(context).primaryColor,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(
+              Icons.clear,
+              color: Theme.of(context).primaryColor,
+            ),
+            onPressed: clearSearch,
+          ),
+        ),
+        onFieldSubmitted: handleSearch,
+      ),
+    );
+  }
+
+  buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        // use list view here so the widget can resize when keyboard open and avoid overflow pixels
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/images/search.svg',
+              height: orientation == Orientation.portrait ? 300.0 : 200.0,
+            ),
+            Text(
+              'Find users',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                fontSize: 60.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildSearchResult() {
+    return FutureBuilder(
+      future: searchResultFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        List<Text> searchResults = [];
+        snapshot.data.document.forEach((doc) {
+          User user = User.fromDocument(doc);
+          searchResults.add(Text(user.username));
+        });
+        return ListView(
+          children: searchResults,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+      appBar: buildSearchFiedl(),
+      body: searchResultFuture == null ? buildNoContent() : buildSearchResult(),
+    );
   }
 }
 
